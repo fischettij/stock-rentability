91b40f56111b55e575ba80643bdd85894f0b222e
fischettij <fischettij@users.noreply.github.com>
2020-04-26 16:24:17 -0300

    Credit card oop (#3)
    * Credit card with first tests
    
    * change test folder
    
    * test api credit cards
    
    * :construction_worker: Travis CI
    
    * fix test:ci

---
+++ src/model/creditCard.js
@@ -0,0 +1,31 @@
+class CreditCard {
+  constructor(bank, company) {
+    this.bank = bank;
+    this.company = company;
+    this.paymentsByMonth = new Map()
+  }
+  paymentsOn(date) {
+    var keyDate = this._dateToStringKey(date)
+    if (this.paymentsByMonth.has(keyDate)) {
+      return this.paymentsByMonth.get(keyDate)
+    } else {
+      return []
+    }
+  }
+
+  addPayment(payment) {
+    var keyDate = this._dateToStringKey(payment.date)
+    if (this.paymentsByMonth.has(keyDate)) {
+      this.paymentsByMonth.get(keyDate).push(payment)
+    } else {
+      this.paymentsByMonth.set(keyDate, [payment])
+    }
+  }
+
+  // Private methods
+  _dateToStringKey(date) {
+    return `${date.getFullYear()}-${date.getMonth() + 1}-1)`
+  }
+}
+
+module.exports.CreditCard = CreditCard
