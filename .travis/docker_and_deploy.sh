1b39dcd3c722435e8fd979418fcd69fba1fa8c83
Julian Fischetti <fische>
2020-06-21 19:28:19 -0300

    fix buidl script

--- .travis/docker_and_deploy.sh
+++ .travis/docker_and_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "${TRAVIS_BRANCH}" = "docker" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     echo "*** Building Docker Containers ***"
     docker build -f docker/Dockerfile -t fischettij/tarjetear-back .
 
@@ -8,9 +8,6 @@ if [ "${TRAVIS_BRANCH}" = "docker" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; 
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     docker push fischettij/tarjetear-back:latest
 
-    # echo "*** Deploying on Server ***"
-    # echo "$SERVER_IP ecdsa-sha2-nistp256 $SERVER_KEY" >> ~/.ssh/known_hosts
-    # ssh $SERVER_USERNAME@$SERVER_IP "/home/$SERVER_USERNAME/unqdifi/covid-back-node-deploy.sh"
 else
     echo "Not deploying, since this branch isn't master or it's a Pull Request."
 fi
